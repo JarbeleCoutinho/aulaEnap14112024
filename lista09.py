4ec19eeb8740f6373db2ea1765cc94796ac290ef
JarbeleCoutinho <jarbele.coutinho@ufersa.edu.br>
2024-11-14 10:00:22 -0300

    lista09.py

---
+++ lista09.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'nomeServidor': ['Adriana', 'Monica', 'Samara'],
+    'salario': [1200,300,5000]
+})
+
+st.write("Criando uma tabela!")
+st.write(df)
+opcao = st.selectbox(
+    'Qual servidor você gostaria de selecionar?', options=["Selecione..."] + df['nomeServidor'].tolist())
+
+st.write('Você selecionou: ', opcao)
+
+dadosFiltrados = df[df['nomeServidor'] == opcao]
+st.write(dadosFiltrados)
